c8c1917b9eddb16657c8e632afe87da64c8ba10c
toocale <137311482+toocale@users.noreply.github.com>
2024-10-27 15:36:31 +0300

    Add files via upload

---
+++ config1.py
@@ -0,0 +1,14 @@
+ALCHEMY_API_KEY = ""
+
+# Alchemy Base Mainnet API URL (replace YOUR_ALCHEMY_API_KEY with your actual key)
+alchemy_base_mainnet_url = f"https://base-mainnet.g.alchemy.com/v2/{ALCHEMY_API_KEY}"
+private_key = ""
+wallet_address= ""
+network = "https://api.basescan.org/api"
+factory_address = "0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6"
+router_address = "0x4752ba5dbc23f44d87826276bf6fd6b1c372ad24"
+swap_methodid = ""
+addliquidity_methodid = "0xf305d719"
+removeliquidity_methodid = "0x02751cec"
+lockliquidity_methodid = "0xeb35ed62"
+explorer_api=''
